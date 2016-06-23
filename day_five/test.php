0b4c25b59659f233506e27a49b338874d5efb710
vslinks <vslinks@qq.com>
2016-06-23 15:15:25 +0800

    几天的工作

--- day_five/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(empty(!$_POST)){
-    //>>进行post 提交
-    echo json_encode($_POST);
-}
-if(!empty($_GET)){
-    echo json_encode($_GET);
-}
-
--- day_one/excel/demo.php
+++
@@ -1,5 +0,0 @@
-<?php
-//>>引入类文件
-require 'MeExcel.class.php';
-$meExcel = new MeExcel();
-$meExcel->test();
